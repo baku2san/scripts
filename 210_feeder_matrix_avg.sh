5d8145889df901c1ff48a86d0deeee409bc207ee
shima <t.oshima@jmtech.co.jp>
2017-07-18 08:03:04 +0900

    Revert "修正記録用のCommit.この後ですぐ戻す予定"
    This reverts commit e89c3a0b737835ed40fbbca7b33ff8d1926cad17.

--- 210_feeder_matrix_avg.sh
+++ 210_feeder_matrix_avg.sh
@@ -10,114 +10,73 @@
 # 2017. 6. 6 maegawa@KSK 修正
 
 # 入力ディレクトリ/ファイル
-iDir=../Cut/TraceReport
+iDir=../../../data/maihime/TraceReport
 iFile=$iDir/MountQualityTrace.csv
 
 # 出力ディレクトリ
-oDir=out0707/tables
+oDir=out/210_feeder_matrix_avg
 mkdir -p $oDir
 
 #RCG,TCの絶対値を先に計算するか(abs/noabs)
 zettaichi=abs
 
-columnsTarget=RCGX,RCGY,RCGA,TCX,TCY,MNTCX,MNTCY,MNTCA,TLX,TLY
-columnsBase=Node,ReelID,datetime,File,FAdd,FSAdd,Head,NHAdd,$columnsTarget
-isOutput=test
-block1=do
-block1=do
-if [ $isOutput ];then
-  xx1=xx1.csv
-  xx2=xx2.csv
-  xx3=xx3.csv
-else
-  xx1=`mktemp`
-  xx2=`mktemp`
-  xx3=`mktemp`
-fi
-
 # MountQualityTrace からフィーダアドレス、同サブアドレス、ノズルヘッドアドレスを取り出す
 # ヘッド番号はデータにないので、フィーダアドレスの先頭 1 桁を流用する
-if [ $block1 ];then
-mcal i=$iFile c='left($s{FAdd},1)' a=Head |
-  mcal c='regexsfx($s{File}, "~")' a=fileInfo |
-  mcal c='regexpfx($s{fileInfo}, "(-[^-]+){6}\\.")' a=datetime |
-  mcut f=$columnsBase o=$xx1
-fi
+mcal i=$iFile c='mid($s{File},46,2)' a=configuration |
+mcal c='left($s{FAdd},1)' a=Head |
+mcut f=configuration,FAdd,FSAdd,Head,NHAdd,RCGX,RCGY,RCGA,TCX,TCY,MPosiRecX,MPosiRecY,MPosiRecA,MPosiRecZ,MNTCX,MNTCY,MNTCA,TLX,TLY o=xx1
+
 # RCGA/MNTCA について、90°付近のものは-90する等
-#mcal i=$xx1 c='if(${RCGA}>=80,${RCGA}-90,${RCGA})' a=RCGAf o=$xxx0
-#mcal i=$xx1 c='if(${RCGA}>160,${RCGA}-180,if(${RCGA}>70,${RCGA}-90,if(${RCGA}<-160,${RCGA}+180,if(${RCGA}<-70,${RCGA}+90,${RCGA}))))' a=RCGAf |
-#mcal          c='if(${MNTCA}>160,${MNTCA}-180,if(${MNTCA}>70,${MNTCA}-90,if(${MNTCA}<-160,${MNTCA}+180,if(${MNTCA}<-70,${MNTCA}+90,${MNTCA}))))' a=MNTCAf o=$xx2
-xx2=$xx1
+#mcal i=xx1 c='if(${RCGA}>=80,${RCGA}-90,${RCGA})' a=RCGAf o=xxx0
+mcal i=xx1 c='if(${RCGA}>160,${RCGA}-180,if(${RCGA}>70,${RCGA}-90,if(${RCGA}<-160,${RCGA}+180,if(${RCGA}<-70,${RCGA}+90,${RCGA}))))' a=RCGAf |
+mcal          c='if(${MNTCA}>160,${MNTCA}-180,if(${MNTCA}>70,${MNTCA}-90,if(${MNTCA}<-160,${MNTCA}+180,if(${MNTCA}<-70,${MNTCA}+90,${MNTCA}))))' a=MNTCAf o=xx2
 
 # RCG,TCについて絶対値を取る
-if [ "$zettaichi" = "abs" ]; then
-    columnsTarget=$columnsTarget
-    columnsTargetCut=RCGA,MNTCA    # 距離計算時に無意味になるのでこれ以外は停止
-    for column in ${columnsTargetCut//,/ }; do
-      columnNames=${columnNames},${column}_:${column}
-    done
-    columnNames=${columnNames:1}
-    echo $columnNames
-#    mcal    c='abs(${RCGX})' a=RCGX_ |
-#    mcal    c='abs(${RCGY})' a=RCGY_ |
-    mcal    c='abs(${RCGA})' a=RCGA_ i=$xx2 |
-#    mcal    c='abs(${TCX})' a=TCX_ |
-#    mcal    c='abs(${TCY})' a=TCY_ |
-#    mcal    c='abs(${MNTCX})' a=MNTCX_ |
-#    mcal    c='abs(${MNTCY})' a=MNTCY_ |
-    mcal    c='abs(${MNTCA})' a=MNTCA_ |
-#    mcal    c='abs(${TLX})' a=TLX_ |
-#    mcal    c='abs(${TLY})' a=TLY_ |
-    mcut -r f=${columnsTargetCut} |
-    mfldname f=${columnNames} o=$xx3
-
+if [ "_"$zettaichi = "_abs" ]; then
+    mcal    c='abs(${RCGX})' a=RCGX_ i=xx2 |
+    mcal    c='abs(${RCGY})' a=RCGY_ |
+    mcal    c='abs(${RCGAf})' a=RCGA_ |
+    mcal    c='abs(${TCX})' a=TCX_ |
+    mcal    c='abs(${TCY})' a=TCY_ |
+    mcal    c='abs(${MPosiRecX})' a=MPosiRecX_ |
+    mcal    c='abs(${MPosiRecY})' a=MPosiRecY_ |
+    mcal    c='abs(${MPosiRecA})' a=MPosiRecA_ |
+    mcal    c='abs(${MPosiRecZ})' a=MPosiRecZ_ |
+    mcal    c='abs(${MNTCX})' a=MNTCX_ |
+    mcal    c='abs(${MNTCY})' a=MNTCY_ |
+    mcal    c='abs(${MNTCAf})' a=MNTCA_ |
+    mcal    c='abs(${TLX})' a=TLX_ |
+    mcal    c='abs(${TLY})' a=TLY_ o=xx3.csv
 else
-  xx3=$xx2
+    mcut i=xx2 f=configuration,FAdd,FSAdd,Head,NHAdd,RCGX:RCGX_,RCGY:RCGY_,RCGAf:RCGA_,TCX:TCX_,TCY:TCY_,MPosiRecX:MPosiRecX_,MPosiRecY:MPosiRecY_,MPosiRecA:MPosiRecA_,MPosiRecZ:MPosiRecZ_,MNTCX:MNTCX_,MNTCY:MNTCY_,MNTCA:MNTCA_,TLX:TLX_,TLY:TLY_ o=xx3.csv
 fi
 
-# -- 結局、PartsNameと、Errorの両方を引っ張る必要があるので、それを考慮して最構築必要
-#   mpf_error にdatetime 入れて、ここでErrorRateを弾けるようにするか
-#   ReelID2ErrorsでError計算して、ここで引くか
-#     そもそもなんでmpf_error 作ったんだ？？ datetime のR/T間での変換だっけか？
-#     そこを考えれば解は見えるかな
-# PartsName ごとの集計
-# 100で出力されたエラー率のファイルと結合する
+# フィーダごとの集計
 # mc04を取り除く
+# 100で出力されたエラー率のファイルと結合する
+# configuration 40 を取り除く
 # X,Yについて、√X^2+Y^2を計算する
-outputTable=${oDir}/ReelID2PartsName.csv
-outputError=${oDir}/ReelID2Errors.csv
-mjoin -n i=$xx3 m=$outputError k=Node,ReelID,datetime f=PartsName, |
-mavg  k=Node,PartsName f=$columnsTarget |
-mcut   f=Node,PartsName,$columnsTarget |
-#mselstr -r f=FAdd v=0 |
-mselstr -r f=Node v=04 |
-msortf f=Node,PartsName |
-mcal c='sqrt(${RCGX}^2+${RCGY}^2)' a=RCGL |
-mcal c='sqrt(${TCX}^2+${TCY}^2)' a=TCL |
-mcal c='sqrt(${MNTCX}^2+${MNTCY}^2)' a=MNTCL |
-mcal c='sqrt(${TLX}^2+${TLY}^2)' a=TLL o=$oDir/avg_PartsName_${zettaichi}.csv
-exit
-mnrjoin -n i=$xx3 m=$outputTable r=datetime R=datetime,datetime1 k=Node,ReelID f=PartsName |
-mavg  k=Node,PartsName f=$columnsTarget |
-mcut   f=Node,PartsName,$columnsTarget |
-#mselstr -r f=FAdd v=0 |
-mselstr -r f=Node v=04 |
-msortf f=Node,PartsName |
-mcal c='sqrt(${RCGX}^2+${RCGY}^2)' a=RCGL |
-mcal c='sqrt(${TCX}^2+${TCY}^2)' a=TCL |
-mcal c='sqrt(${MNTCX}^2+${MNTCY}^2)' a=MNTCL |
-mcal c='sqrt(${TLX}^2+${TLY}^2)' a=TLL o=$oDir/avg_PartsName_${zettaichi}.csv
-
+mavg  i=xx3.csv k=configuration,FAdd,FSAdd f=RCGX_,RCGY_,RCGA_,TCX_,TCY_,MPosiRecX_,MPosiRecY_,MPosiRecA_,MPosiRecZ_,MNTCX_,MNTCY_,MNTCA_,TLX_,TLY_ |
+mcut   f=configuration,FAdd,FSAdd,RCGX_,RCGY_,RCGA_,TCX_,TCY_,MPosiRecX_,MPosiRecY_,MPosiRecA_,MPosiRecZ_,MNTCX_,MNTCY_,MNTCA_,TLX_,TLY_ |
+mselstr -r f=FAdd v=0 |
+mselstr -r f=configuration v=04 |
+msortf f=configuration,FAdd,FSAdd |
+mcal c='sqrt(${RCGX_}^2+${RCGY_}^2)' a=RCGL |
+mcal c='sqrt(${TCX_}^2+${TCY_}^2)' a=TCL |
+mcal c='sqrt(${MPosiRecX_}^2+${MPosiRecY_}^2)' a=MPosiRecL |
+mcal c='sqrt(${MNTCX_}^2+${MNTCY_}^2)' a=MNTCL |
+mcal c='sqrt(${TLX_}^2+${TLY_}^2)' a=TLL |
+mjoin -n k=configuration,FAdd,FSAdd f=errorRate m=out/100_countMountPickupFeeder/mpf_error_by_feeder.csv o=$oDir/avg_feeder_${zettaichi}.csv
 
 # ノズルごとの集計
-#mavg   i=$xx0 k=Node,Head,NHAdd f=RCGX,RCGY,RCGA,TCX,TCY,MPosiRecX,MPosiRecY,MPosiRecA,MPosiRecZ,MNTCX,MNTCY,MNTCA,TLX,TLY |
-#mcut   f=Node,Head,NHAdd,RCGX,RCGY,RCGA,TCX,TCY,MPosiRecX,MPosiRecY,MPosiRecA,MPosiRecZ,MNTCX,MNTCY,MNTCA,TLX,TLY |
-#msortf f=Node,Head,NHAdd%n o=$oDir/rcgtc_nozzle.csv
+#mavg   i=xx0.csv k=configuration,Head,NHAdd f=RCGX_,RCGY_,RCGA_,TCX_,TCY_,MPosiRecX_,MPosiRecY_,MPosiRecA_,MPosiRecZ_,MNTCX_,MNTCY_,MNTCA_,TLX_,TLY_ |
+#mcut   f=configuration,Head,NHAdd,RCGX_,RCGY_,RCGA_,TCX_,TCY_,MPosiRecX_,MPosiRecY_,MPosiRecA_,MPosiRecZ_,MNTCX_,MNTCY_,MNTCA_,TLX_,TLY_ |
+#msortf f=configuration,Head,NHAdd%n o=$oDir/rcgtc_nozzle.csv
 
 # フィーダ x ノズルで集計
-#mavg   i=$xx0 k=Node,FAdd,FSAdd,Head,NHAdd f=RCGX,RCGY,RCGA,TCX,TCY,MPosiRecX,MPosiRecY,MPosiRecA,MPosiRecZ,MNTCX,MNTCY,MNTCA,TLX,TLY|
-#mcut   f=Node,FAdd,FSAdd,Head,NHAdd,RCGX,RCGY,RCGA,TCX,TCY,MPosiRecX,MPosiRecY,MPosiRecA,MPosiRecZ,MNTCX,MNTCY,MNTCA,TLX,TLY |
-#msortf f=Node,FAdd,FSAdd,Head,NHAdd%n o=$oDir/rcgtc_feeder_nozzle.csv
+#mavg   i=xx0.csv k=configuration,FAdd,FSAdd,Head,NHAdd f=RCGX_,RCGY_,RCGA_,TCX_,TCY_,MPosiRecX_,MPosiRecY_,MPosiRecA_,MPosiRecZ_,MNTCX_,MNTCY_,MNTCA_,TLX_,TLY_|
+#mcut   f=configuration,FAdd,FSAdd,Head,NHAdd,RCGX_,RCGY_,RCGA_,TCX_,TCY_,MPosiRecX_,MPosiRecY_,MPosiRecA_,MPosiRecZ_,MNTCX_,MNTCY_,MNTCA_,TLX_,TLY_ |
+#msortf f=configuration,FAdd,FSAdd,Head,NHAdd%n o=$oDir/rcgtc_feeder_nozzle.csv
 
 
 
